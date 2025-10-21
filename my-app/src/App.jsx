55a3abced4c299109c84e1df664e51dd2652d53a
Mohammad Tariq <ariqxd01@gmail.com>
2025-10-21 23:27:46 +0530

    Project setup react app

---
+++ my-app/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+      <h1>Hello guys | Mohammad Tariq learning React 🚀</h1>
+      <button>
+        Click me
+      </button>
+      <p>
+        hello world
+      </p>
+    </>
+  )
+}
