9cffb2eed3ad3781df409569763938bacfefb2a1
Mohammad Tariq <ariqxd01@gmail.com>
2025-10-22 23:22:59 +0530

    day 2

--- 01_React_Basics/src/App.jsx
+++ 01_React_Basics/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Item from './Componets/item'
 
 export default function App() {
   return (
@@ -10,6 +11,9 @@ export default function App() {
       <p>
         hello world
       </p>
+      <Item />
+      <h2>Today we learned that the function build should always start with a capital letter</h2>
+      <h3>The js injection happens manually in vite + react but in react default it gets already connected not a single line of code written there </h3>
     </>
   )
 }
---
+++ 01_React_Basics/src/Componets/item.jsx
@@ -0,0 +1,5 @@
+export default function Item() {
+  return (
+      <h1> This is day 2 of learing react </h1>
+  )
+}
