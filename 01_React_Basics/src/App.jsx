84a4eac98aa7a55e471ceb3a43ef1a5b1bbe4bee
Mohammad Tariq <ariqxd01@gmail.com>
2025-10-24 01:06:10 +0530

    react under the hood

--- 01_React_Basics/src/App.jsx
+++ 01_React_Basics/src/App.jsx
@@ -14,6 +14,17 @@ export default function App() {
       <Item />
       <h2>Today we learned that the function build should always start with a capital letter</h2>
       <h3>The js injection happens manually in vite + react but in react default it gets already connected not a single line of code written there </h3>
+
+      <h1>Day 3</h1>
+      <p>so we build custom react for ourself and used it and learned a few things so here goes-</p>
+      <p>1) we can't declare function in main file</p>
+      <p>2) we can declare a function and but we use it as {} inside the paraenthese much like backticks `${}`</p>
+      <p>3) we can't use if else or any manipulation inside the paranthesis{} because it is like a object and we can't do manipulation in object we must do this outside</p>
+      <p>4) just like we created custom react react also do convert the jsx to type and all but in different syntax</p>
+      <p>5) react use babel to convert jsx to js</p>
+      <p>6) react use createElement to convert jsx to js</p>
+      <p>7) react use render to render the element on the screen</p>
+      
     </>
   )
 }
--- 01_React_Basics/src/main.jsx
+++ 01_React_Basics/src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+// Creating a React element manually (without JSX)
+const reactElement = React.createElement(
+  "a",
+  { href: "https://google.com", target: "_blank" },
+  "Click here to go to Google"
+);
+const anotherelement = (
+  <a href="https://google.com"> click here</a>
 )
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <>
+    <App />
+    {reactElement}
+    {anotherelement}
+  </>
+);
