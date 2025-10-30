9abeed655a28a0723eecda9619d692adb4905313
Mohammad Tariq <ariqxd01@gmail.com>
2025-10-30 23:54:32 +0530

    first commit

---
+++ 06_Currency_converter/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 >Currency Converter</h1>
+    </>
+  )
+}
+
+export default App
+// api -> https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json
