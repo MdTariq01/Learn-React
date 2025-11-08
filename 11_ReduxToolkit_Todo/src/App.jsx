e8439503959801ab37d112d875416dd751ca2565
Mohammad Tariq <ariqxd01@gmail.com>
2025-11-08 23:19:45 +0530

    first commit

---
+++ 11_ReduxToolkit_Todo/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { store } from '../app/store'
+import { Provider } from 'react-redux'
+function App() {
+  
+  return (
+    <>
+      <h1 className='bg-red-700 text-3xl text-center m-10 p-10 text-white'>Learing Redux Toolkit</h1>
+    </>
+  )
+}
+
+export default App
