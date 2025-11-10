dd4b22ef084102fa92ecb9efdeb9f00664c1043e
Mohammad Tariq <ariqxd01@gmail.com>
2025-11-10 21:19:35 +0530

    Appwrite Setup

---
+++ 12_Mega-Blog/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+    console.log(import.meta.env.VITE_APPWRITE_URL)
+
+  return (
+    <>
+      <h1 className='bg-red-700 p-5 m-5 text-center text-2xl text-white'>Hello Ji</h1>
+    </>
+  )
+}
+
+export default App
