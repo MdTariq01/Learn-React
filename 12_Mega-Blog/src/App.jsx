4d06b028e72b980b850b45c6ef9c95c08be1b410
Mohammad Tariq <ariqxd01@gmail.com>
2025-11-21 23:14:27 +0530

    Store Configure

--- 12_Mega-Blog/src/App.jsx
+++ 12_Mega-Blog/src/App.jsx
@@ -1,13 +1,38 @@
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
+import authService from './appwrite/auth'
+import  {login , logout} from './store/authSlice'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
 function App() {
-    console.log(import.meta.env.VITE_APPWRITE_URL)
-
-  return (
-    <>
-      <h1 className='bg-red-700 p-5 m-5 text-center text-2xl text-white'>Hello Ji</h1>
-    </>
-  )
+  const [loading , setLoading] = useState(true)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    authService.getCurrentUser().then((userData) => {
+      if(userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => {
+      setLoading(false)
+    })
+  } , [] )
+  
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+          <main>
+            todo :  {/* <Outlet /> */}
+          </main>
+        <Footer />
+      </div>
+    </div>
+  ) : (null)
 }
-
+ 
 export default App
--- 12_Mega-Blog/src/store/authSlice.js
+++ 12_Mega-Blog/src/store/authSlice.js
@@ -1,7 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const authSlice = {
-  initialState : {
-    'authenticaton'
-  }
+const initialState = {
+  status_ : false ,
+  userData : null
 }
+
+const authSlice = createSlice({
+  name : 'auth' ,
+  initialState ,
+  reducers : {
+    login : (state, action) => {
+      state.status_ = true;
+      state.userData = action.payload.userData;
+    } ,
+    logout : (state) => {
+      state.status_ = false ;
+      state.userData = null;
+    }
+  }
+})
+
+export const { login , logout } = authSlice.actions;
+export default authSlice.reducer;
